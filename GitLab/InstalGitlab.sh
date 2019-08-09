cef925d3d852022c95aa51cf85a3c8c33e5b32e9
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-08-09 16:28:44 +0200

    Update InstalGitlab.sh

--- GitLab/InstalGitlab.sh
+++ GitLab/InstalGitlab.sh
@@ -3,3 +3,9 @@ $ su -c 'apt-get install curl'
 
 # Instalation de GitLab
 $ curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+
+# Configure  gitlab
+$ sudo gitlab-ctl reconfigure
+
+# Il faut modifier ce fichier pour les paths des repositories
+$ nano /etc/gitlab/gitlab.rb
