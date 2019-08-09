d2b8cd7ae646fb89dffffc1479e6200965593452
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-08-09 15:28:11 +0200

    Create InstalGitlab.sh

---
+++ GitLab/InstalGitlab.sh
@@ -0,0 +1,5 @@
+# Instalaltion de Curl 
+$ su -c 'apt-get install curl'
+
+# Instalation de GitLab
+$ curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
