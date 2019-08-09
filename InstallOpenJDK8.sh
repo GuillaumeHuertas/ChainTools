dc0262ee9928fe6a65fb623c5a35f633d29a39a0
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-08-09 13:36:08 +0200

    Update InstallOpenJDK8.sh

--- InstallOpenJDK8.sh
+++ InstallOpenJDK8.sh
@@ -12,3 +12,29 @@ $ su -c 'ln -s /opt/java-se-8u40-ri/ /opt/jdk-8'
 $ /opt/jdk-8/bin/java -version
 # Vérifie la version de Javac
 $ /opt/jdk-8/bin/javac -version
+
+# Option 1
+# Connection su
+$ su
+# Création du script javamaven.sh
+$ touch /etc/profile.d/javamaven.sh
+# Attribution des droits pour l'exécution 
+$ chmod 755 /etc/profile.d/javamaven.sh
+# Les lignes ajoutés au javamaven.sh
+# JAVA_HOME=/opt/jdk-8
+# PATH=${JAVA_HOME}/bin:${PATH}
+
+# export JAVA_HOME PATH
+
+# Option 2
+# Comme Option 1 en une seule commande
+su -c 'echo -e "JAVA_HOME=/opt/jdk-8 \nPATH=${JAVA_HOME}/bin:${PATH} \n\nexport JAVA_HOME PATH" \
+| tee -a /etc/profile.d/javamaven.sh \
+&& chmod 755 /etc/profile.d/javamaven.sh'
+
+# Version de java 
+$ java -version
+# Version javac
+$ javac -version
+
+
