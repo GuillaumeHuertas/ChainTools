0c89d153a24e30878d4926f845ee43e5a1a63a90
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-08-12 15:59:41 +0200

    Update InstallGitLabRaspian.sh

--- Raspberry/InstallGitLabRaspian.sh
+++ Raspberry/InstallGitLabRaspian.sh
@@ -1,4 +1,23 @@
-# Instalaltion de GitLab et Docker sur une image Raspbian Buster
+# Instalaltion de GitLab et Docker sur une image Raspbian Buster Full
 
 # Changer le clavier >> fr à la place de en
 $ sudo nano /etc/default/keyboard
+
+# Installer Docker 
+$ curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+# Ajouter l'utilisateur au groupe Docker
+$ sudo usermod -aG docker pi
+
+# Installer GitLab
+$ sudo apt install curl openssh-server ca-certificates postfix apt-transport-https
+# Choisir "Site internet"
+# Ajouter les clées de dépôt
+$ curl https://packages.gitlab.com/gpg.key | sudo apt-key add -
+# Ajout du dépôt 
+$ sudo curl -o /etc/apt/sources.list.d/gitlab_ce.list "https://packages.gitlab.com/install/repositories/gitlab/raspberry-pi2/config_file.list?os=debian&dist=jessie" && sudo apt-get update
+# Installe GitLab Communauty Edition
+$ sudo apt-get install gitlab-ce
+# Reconfigure GitLab
+$ sudo gitlab-ctl reconfigure
+# Se connecter à votre adresse local 
+# Par défaut le login est « root » et le mot de passe « 5iveL!fe »
