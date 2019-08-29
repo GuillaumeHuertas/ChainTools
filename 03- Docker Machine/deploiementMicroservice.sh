88bc6eb0050802b8585a950da85fca0fa536c317
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-08-29 10:31:29 +0200

    Update deploiementMicroservice.sh

--- 03- Docker Machine/deploiementMicroservice.sh
+++ 03- Docker Machine/deploiementMicroservice.sh
@@ -1,7 +1,7 @@
 
 # Créé un overlay de réseau (surcouche)
 $ docker network create --driver overlay reseau
-# Créé un service conteneur db basé sur l'image mongo:3.2 qui se connecte sur l'overlay reseau
+# Instancie service conteneur db basé sur l'image mongo:3.2 qui se connecte sur l'overlay reseau
 $ docker service create --name db --network reseau mongo:3.2
-# Créé un service conteneur app basé sur l'image node-app qui se connecte sur l'overlay reseau
+# Instancie service conteneur app basé sur l'image node-app qui se connecte sur l'overlay reseau
 $ docker service create --name app --network reseau -- publish 8080:80 node-app
