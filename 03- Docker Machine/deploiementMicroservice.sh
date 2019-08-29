b81b3c253d0e5283ff4f80158a4e930f9a588de9
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-08-29 14:32:33 +0200

    Update deploiementMicroservice.sh

--- 03- Docker Machine/deploiementMicroservice.sh
+++ 03- Docker Machine/deploiementMicroservice.sh
@@ -25,15 +25,62 @@ DELETE / 200 63.408 ms - -
 GET / 200 18.609 ms - 1
 DELETE / 200 39.585 ms - -
 
- # Liste les services 
-  $ docker service ls
+# Liste les services 
+$ docker service ls
   
-  # Changement d'échelle
-  $ docker service scale app=3
+# Changement d'échelle
+$ docker service scale app=3
   
-  # Supprime les containers 
-  $ docker service rm db app
-  # Supprime le réseau
-  $ docker network rm reseau
+# Supprime les containers 
+$ docker service rm db app
+# Supprime le réseau
+$ docker network rm reseau
   
-   Installation automatisé
+Installation automatisé
+
+# Lancer une stack 
+$ docker stack deploy -c ./docker-compose.yml mastack
+# taguer l'image node-app
+$ docker tag node-app guillaumehuertas/sample-app-1:1.0
+# Vérifier la présence de l'image taguer 
+$ docker images 
+
+REPOSITORY                      TAG                 IMAGE ID            CREATED             SIZE
+guillaumehuertas/sample-app-1   1.0                 733d09a235c6        22 hours ago        726MB
+node-app                        latest              733d09a235c6        22 hours ago        726MB
+mongo                           3.2                 fb885d89ea5c        9 months ago        300MB
+node                            0.12.4              bc8475902924        4 years ago         712MB
+
+# Se connecter à DockerHub
+$ docker login 
+# Entrez vos ID DockerHub
+
+Login with your Docker ID to push and pull images from Docker Hub. If you don't have a Docker ID, head over to https://hub.docker.com to create one.
+Username: guillaumehuertas
+Password:
+WARNING! Your password will be stored unencrypted in /home/docker/.docker/config.json.
+Configure a credential helper to remove this warning. See
+https://docs.docker.com/engine/reference/commandline/login/#credentials-store
+
+Login Succeeded
+
+# Envoyer l'image sur DockerHub
+$ docker push guillaumehuertas/sample-app-1:1.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
