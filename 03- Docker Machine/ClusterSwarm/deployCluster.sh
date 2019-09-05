f55d64888b39cb6d9746d7c6705db5929e0021a4
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-09-05 11:26:15 +0200

    Create deployCluster.sh

---
+++ 03- Docker Machine/ClusterSwarm/deployCluster.sh
@@ -0,0 +1,9 @@
+Il faut au préalable faire un cluster avec un Manager et deux Agents
+
+
+# Créer et se déplacer dans le dossier DevOps
+$ mkdir DevOps && cd DevOps
+# Copier le repository git avec le projet sample-app-
+$ git clone https://github.com/EditionsENI/sample-app-1.git-1
+# Se mettre en version 1.0 du projet
+$ git checkout v1.0
