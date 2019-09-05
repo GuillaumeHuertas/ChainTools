13b7359cb421ec9f0711925d0fd39301eb64f5c4
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-09-05 11:29:36 +0200

    Update deployCluster.sh

--- 03- Docker Machine/ClusterSwarm/deployCluster.sh
+++ 03- Docker Machine/ClusterSwarm/deployCluster.sh
@@ -1,17 +1,17 @@
-Il faut au préalable faire un cluster avec un Manager et deux Agents
+#Il faut au préalable faire un cluster avec un Manager et deux Agents
 
 
 # Créer et se déplacer dans le dossier DevOps
-$ mkdir DevOps && cd DevOps
+mkdir DevOps && cd DevOps
 # Copier le repository git avec le projet sample-app-
-$ git clone https://github.com/EditionsENI/sample-app-1.git-1
+git clone https://github.com/EditionsENI/sample-app-1.git-1
 # Se mettre en version 1.0 du projet
-$ git checkout v1.0
+git checkout v1.0
 
 # Vérifier que tous les noeuds sont bien présents
-$ docker node ls
+docker node ls
 # Récupérer l'image MongoDB
-$ docker pull mongo:3.2
+docker pull mongo:3.2
 # Se déplace dans l'appli
- cd node-app
+cd node-app
 
