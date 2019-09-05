61a4726a5a2a822212bad648f60fe1b0e051b708
GuillaumeHuertas <49951461+GuillaumeHuertas@users.noreply.github.com>
2019-09-05 11:34:48 +0200

    Update deployCluster.sh

--- 03- Docker Machine/ClusterSwarm/deployCluster.sh
+++ 03- Docker Machine/ClusterSwarm/deployCluster.sh
@@ -1,6 +1,6 @@
 #Il faut au préalable faire un cluster avec un Manager et deux Agents
 
-
+echo "Déploiment manuel"
 # Créer et se déplacer dans le dossier DevOps
 mkdir DevOps && cd DevOps
 # Copier le repository git avec le projet sample-app-
@@ -10,8 +10,12 @@ git checkout v1.0
 
 # Vérifier que tous les noeuds sont bien présents
 docker node ls
+
+echo "Téléchargement et comîlation des images"
 # Récupérer l'image MongoDB
 docker pull mongo:3.2
 # Se déplace dans l'appli
 cd node-app
+# Compiler l'image correspandante en lui donner un nom (tag)
+docker build -t node-app .
 
